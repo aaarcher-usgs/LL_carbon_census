d68cd2a35bda3056cb1bb3d979285b98e5793332
aaarchmiller <althea.archmiller@gmail.com>
2018-08-13 10:58:28 -0500

    calculations for total biomass by subplot

--- programs/summer2018/01_data_processing.R
+++ programs/summer2018/01_data_processing.R
@@ -2,28 +2,57 @@
 #' 
 #' description
 #' 
+remove(list=ls())
+library(reshape)
 
-
-getwd()
-bsdata<- (read.csv("Raw_Data/Biomass_Sorting_Data.csv"))
-
+#' Load biomass table
+bsdata <- read.csv("Raw_Data/Biomass_Sorting_Data.csv")
 head(bsdata)
 
 #' Calculate biomass weight
-
 bsdata$biomass_wt <- (bsdata$dried_wt - bsdata$bag_wt)
 
+
 #' Select prairie plots from data
+prairie_plots <- c(2,5,11,15,16,20,18,21,23,24,25,27)
+prairie_data <- bsdata[bsdata$plot%in%prairie_plots,]
 
-prairie_plots<- c(2,5,11,15,16,20,18,21,23,24,25,27)
-prairie_data<- bsdata[bsdata$plot%in%prairie_plots,]
+#' Create unique identifier
+#' 
+prairie_data$p_sp_spp <- ifelse(test = prairie_data$plot < 10, 
+                                    yes = paste0("P0", prairie_data$plot, "_SP",  prairie_data$subplot, "_", prairie_data$species),
+                                    no = paste0("P", prairie_data$plot, "_SP", prairie_data$subplot, "_", prairie_data$species))
 
-#' Select subplots to create total biomass
-#' We tried cbind (error stated missing function) and merge (not correct output)
-#'error:cannot find function "%>%"
-#'From Friday...
-prairie_data$plot_subplot<- paste(prairie_data$plot,prairie_data$subplot,sep = '_')
-prairie_data$biomass_total<-aggregate(prairie_data$biomass_wt,list(prairie_data$plot_subplot),sum)
+
+#' We need to merge the two separate biomass bags' worth of data for 
+#' plot 1, subplot 2
+#' 
+aggregate_data_sp <- aggregate(prairie_data$biomass_wt, list(prairie_data$p_sp_spp), sum)
+
+#' Separate p_sp_spp into new columns
+prairie_data <- transform(aggregate_data_sp, 
+                          ID = colsplit(x = Group.1, split = "_", names = c("plot", "subplot", "species")))
+
+#' Rename columns
+colnames(prairie_data)
+colnames(prairie_data) <- c("p_sp_spp", "spp_biomass", "plot", "subplot", "species")
+
+#' Create unique identifier
+#' 
+prairie_data$plot_subplot <- paste(prairie_data$plot, prairie_data$subplot, sep = "_")
+
+#' Summarize biomass data (sum) by plot_subplot
+aggregate_data_plot <- aggregate(prairie_data$spp_biomass, list(prairie_data$plot_subplot), sum)
+
+#' Merge total biomass back to prairie_data
+prairie_data <- merge(x = prairie_data, y = aggregate_data_plot, 
+                      by.x = "plot_subplot", by.y = "Group.1")
+
+#' Rename columns
+colnames(prairie_data)
+colnames(prairie_data) <- c("plot_subplot", "p_sp_spp", "spp_biomass", "plot", "subplot", "species", "total_biomass")
+
+
 library(plyr)
 x%>%
   group_by(prairie_data$subplot)%>%
