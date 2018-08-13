9daeaa2d5408eefcaab6bf0070f95222d0b48ff2
aaarchmiller <althea.archmiller@gmail.com>
2018-08-13 13:23:09 -0500

    set up template for data analysis

---
+++ programs/summer2018/03_data_analysis.R
@@ -0,0 +1,29 @@
+#' # Test hypotheses
+#' 
+#' **Objective** Effect of Spring Burn on Understory Biomass & Diversity
+#' 
+#' Analyzes the effect of burn on various dependent variables.
+#' 
+#' ## Header
+remove(list=ls())
+library(ezknitr)
+library(knitr)
+
+#' ## Load data
+#' 
+#' Subplot-level data
+load(file = "data/processed_data/summer2018_litter_data.R")
+
+#' ## Hypothesis 1: XXX
+#' 
+#' 
+#' 
+
+#' ## Save Results
+#' 
+
+#' ## Footer
+#' 
+#' spun with
+#' ezknitr::ezspin(file = "programs/summer2018/03_data_analysis.R", out_dir = "programs/summer2018/output", fig_dir = "figures", keep_md = F)
+#' 
