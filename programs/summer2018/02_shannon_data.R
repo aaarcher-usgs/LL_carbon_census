1ba227da7f0de600d983e7f877d4830bcab5a80c
aaarchmiller <althea.archmiller@gmail.com>
2018-08-13 13:04:34 -0500

    calculated shannon diversity index

--- programs/summer2018/02_shannon_data.R
+++ programs/summer2018/02_shannon_data.R
@@ -11,7 +11,11 @@ library(knitr)
 
 #' ## Load data
 #' 
+#' Species-level data
 load(file = "data/processed_data/summer2018_prairie_data.R")
+
+#' Subplot-level data
+load(file = "data/processed_data/summer2018_litter_data.R")
 
 #' ## Step 1: Calculate live biomass (exclude litter from analysis)
 #' 
@@ -37,9 +41,37 @@ live_plants <- merge(x = live_plants, y = aggregate_live, by = "plot_subplot")
 aggregate_richness <- aggregate(live_plants$spp_biomass, list(live_plants$plot_subplot), FUN = length)
 colnames(aggregate_richness) <- c("plot_subplot", "richness")
 
-#' Merge back to original live_plants data
-live_plants <- merge(x = live_plants, y = aggregate_richness, by = "plot_subplot")
+#' Add $S_{sp}$ to litter_data
+litter_data <- merge(x = litter_data, y = aggregate_richness, by = "plot_subplot")
 
+#' ## Step 3: Calculate $p_{i_{sp}}$
+#' 
+live_plants$prop_biomass <- live_plants$spp_biomass/live_plants$live_biomass
+
+#' ## Step 4: Calculate $H_{sp}$
+#' 
+#' Using aggregate_richness as a dataframe, we will go through each subplot and calculate
+#' the Shannon Diversity index
+#' 
+for(sp_index in 1:nrow(litter_data)){
+  # Create small dataframe with just that subplot's data
+  temp_df <- live_plants[live_plants$plot_subplot == litter_data$plot_subplot[sp_index],]
+  
+  # Calculate species-level part
+  temp_df$p_lnp <- temp_df$prop_biomass * log(temp_df$prop_biomass)
+
+  # Calculate Shannon diversity index
+  litter_data$shannon[sp_index] <- -1 * sum(temp_df$p_lnp)
+}
+
+#' ## Just for fun, visualize our results
+#+ explore_shannon
+plot(x = litter_data$shannon, y = litter_data$richness)
+
+#' ## Save data
+#' 
+save(prairie_data, file = "data/processed_data/summer2018_prairie_data.R")
+save(litter_data, file = "data/processed_data/summer2018_litter_data.R")
 
 #' ## Footer
 #' 
