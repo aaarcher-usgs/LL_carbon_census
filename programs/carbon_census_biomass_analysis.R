892a40f49f63487057da559a9f71f789d32f6ad9
Marley Lund <marleylund@Marleys-MacBook-Pro.local>
2018-08-13 08:29:20 -0500

    Work from Friday, August 10, 2018

--- programs/carbon_census_biomass_analysis.R
+++ programs/carbon_census_biomass_analysis.R
@@ -1,4 +1,4 @@
-#' #Carbon Census Biomass Analysis
+#' #Effect of the Spring Burn on Understory Biomass and Diversity
 #' 
 #' description
 #' 
@@ -10,3 +10,5 @@ bsdata<- (read.csv("Raw_Data/Biomass_Sorting_Data.csv"))
 head(bsdata)
 #' Calculate biomass weight
 bsdata$biomass_wt <- (bsdata$dried_wt - bsdata$bag_wt)
+
+#' Create data format for analysis
