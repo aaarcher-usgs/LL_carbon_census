eb33e81f8ed28bf4e8466a283a40caad61c76a86
Marley Lund <marleylund@Marleys-MacBook-Pro.local>
2018-08-15 12:07:57 -0500

    possible post processing

--- programs/summer2018/04_post_processing.R
+++ programs/summer2018/04_post_processing.R
@@ -9,7 +9,7 @@
 
 library(ezknitr)
 library(knitr)
-
+library(dplyr)
 
 #' ## Load data
 #' 
@@ -27,10 +27,30 @@ head(bsdata)
 prairie_data$plot_species <- paste(prairie_data$plot, prairie_data$species, sep = "_")
 
 #' Summarize biomass data (sum) by plot_species
-aggregate_data_plot_species <- aggregate(prairie_data$spp_biomass, list(prairie_data$plot_species), sum)
+aggregate_data_plot_species <- aggregate(prairie_data$spp_biomass, 
+                                         list(prairie_data$plot_species), FUN = sum)
+
+#' Renamed columns in the aggregate temporary hold
+colnames(aggregate_data_plot_species) <- c("plot_species", "biomass")
+
+#'Add burn year to summarization
+#'
+species_biomass <- group_by (prairie_data, plot_species)
+
+#'Can we aggragate without having a value? spring_burn is binary
+species_biomass <- aggregate(prairie_data$spring_burn ~ prairie_data$plot_species, df, )
+
+#' Setkey was another attempt at moving one column (spring_burn) over to aggregate dataset
+setkey(setDT(aggregate_data_plot_species), plot_species)
+aggregate_data_plot_species[prairie_data, spring_burn := i.spring_burn]
+
+#' Does merge work for one column or does it have to be the complete dataset?
+aggregate_data_plot_species <- merge(x = aggregate_data_plot_species, y = prairie_data$spring_burn, 
+                                     by = c("plot_species"))
 
 #' ## Save Results
 #' 
+save(prairie_data, file = "data/processed_data/summer2018_prairie_data.R")
 
 #' ## Footer
 #' 
