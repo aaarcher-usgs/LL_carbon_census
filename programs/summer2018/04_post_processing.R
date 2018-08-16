2a7fbd46c99f123ad55f4af30a4e441cf6100298
aaarchmiller <althea.archmiller@gmail.com>
2018-08-16 11:01:49 -0500

    save the live biomass by species data for graphing

--- programs/summer2018/04_post_processing.R
+++ programs/summer2018/04_post_processing.R
@@ -6,7 +6,7 @@
 #' respect to burn year.
 #' 
 #' ## Header
-
+remove(list=ls())
 library(ezknitr)
 library(knitr)
 library(dplyr)
@@ -16,10 +16,6 @@ library(dplyr)
 #' Subplot-level data
 load(file = "data/processed_data/summer2018_prairie_data.R")
 
-#' Load biomass table
-bsdata <- read.csv("data/Raw_Data/Biomass_Sorting_Data.csv")
-head(bsdata)
-
 #' ## Create data set with plot and species
 #' 
 #' #' Create unique identifier
@@ -27,22 +23,27 @@ head(bsdata)
 prairie_data$plot_species <- paste(prairie_data$plot, prairie_data$species, sep = "_")
 
 #' Summarize biomass data (sum) by plot_species
-plot_species_biomass <- aggregate(prairie_data$spp_biomass, 
-                                         list(prairie_data$plot_species, prairie_data$spring_burn), FUN = sum)
+plot_species_biomass <- 
+  aggregate(prairie_data$spp_biomass, 
+            list(prairie_data$plot_species, 
+                 prairie_data$spring_burn,
+                 prairie_data$species,
+                 prairie_data$plot), 
+            FUN = sum)
 
 #' Renamed columns in the aggregate temporary hold
-colnames(plot_species_biomass) <- c("plot_species", "spring_burn", "biomass")
+colnames(plot_species_biomass) <- c("plot_species", "spring_burn", "species", "plot","biomass")
+head(plot_species_biomass)
 
-#' Create graphs to analize data
-#' 
-barplot(
+#' Remove litter
+liveBM_x_spp <- plot_species_biomass[!plot_species_biomass$species=="Litter",]
 
 #' ## Save Results
 #' 
-save(prairie_data, file = "data/processed_data/summer2018_prairie_data.R")
+save(liveBM_x_spp, file = "data/processed_data/liveBM_x_spp.R")
 
 #' ## Footer
 #' 
 #' spun with
-#' ezknitr::ezspin(file = "programs/summer2018/03_data_analysis.R", out_dir = "programs/summer2018/output", fig_dir = "figures", keep_md = F)
+#' ezknitr::ezspin(file = "programs/summer2018/04_post_processing.R", out_dir = "programs/summer2018/output", fig_dir = "figures", keep_md = F)
 #' 
