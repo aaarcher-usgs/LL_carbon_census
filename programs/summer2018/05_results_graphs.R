7d0346c6b46d3fa9cddf29c5f1df48cb7c24592b
echandle2228 <39994664+echandle2228@users.noreply.github.com>
2018-08-17 10:03:45 -0500

    trying out the stacked graph

--- programs/summer2018/05_results_graphs.R
+++ programs/summer2018/05_results_graphs.R
@@ -9,12 +9,21 @@
 remove(list=ls())
 library(ezknitr)
 library(knitr)
+
 library(ggplot2)
+library(ggthemes)
+library(extrafont)
+library(plyr)
+library(scales)
+
 
 #' ## Load data
 #' 
 #' Subplot-level data
 load(file = "data/processed_data/summer2018_prairie_data.R")
+
+#' plot level live species
+load(file = "data/processed_data/summer2018_liveBM_x_spp.R")
 
 #' Biomass by burn and live status
 load(file = "data/processed_data/summer2018_biomass_x_burn.R")
@@ -63,8 +72,18 @@ ggplot(aes(y = sHannon_mean, x = burn_status), data = burn_x_RSH)+
                 width=.2)+
   theme_classic()
 
+#' ## Figure 5
+#' 
+#+ figure5
+ggplot(aes(y = biomass, x = plot), data = liveBM_x_spp)+
+  geom_bar(stat="identity", aes(fill=species)) +
+  scale_fill_manual(values=c("#999999", "#E69F00")) +
+  theme_classic()
+
 #' ## Footer
 #' 
 #' spun with
 #' ezknitr::ezspin(file = "programs/summer2018/05_results_graphs.R", out_dir = "programs/summer2018/output", fig_dir = "figures", keep_md = F)
 #' 
+
+
